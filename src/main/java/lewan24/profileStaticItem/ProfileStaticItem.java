8d2c6738a049f514d936ea592be5ea5a2d324c46
LewanMordor@protonmail.com <LewanMordor@protonmail.com>
2024-10-05 20:12:26 +0200

    Initial commit

---
+++ src/main/java/lewan24/profileStaticItem/ProfileStaticItem.java
@@ -0,0 +1,17 @@
+package lewan24.profileStaticItem;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ProfileStaticItem extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
